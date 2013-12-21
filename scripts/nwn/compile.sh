c012b7b177b984bbdf32666f7a4b1559ea17a607
jmd <jmd2028@gmail.com>
2013-12-21 04:33:40 -0800

    Update compile script to use a diff location.

--- scripts/nwn/compile.sh
+++ scripts/nwn/compile.sh
@@ -4,7 +4,7 @@ CP="nss/*.nss"
 MASK="nss/$1*.nss"
 MOVE="nss/$1*.ncs"
 NCS="ncs/$1*.ncs"
-MOD="C:/NeverwinterNights/NWN/modules/temp0"
+MOD="c:/Users/josh/software/NWN/modules/temp0"
 RM_MOD_NSS="$MOD/*.nss"
 
 echo "$MASK"
@@ -15,6 +15,6 @@ echo $MOD
 rm $NCS
 rm $RM_MOD_NSS
 cp $CP $MOD
-NWNScriptCompiler -1ceglo -n "C:/NeverwinterNights/NWN" -r $MOD "$MASK"
+NWNScriptCompiler -1ceglo -n "c:/Users/josh/software/NWN" -r $MOD "$MASK"
 rm $RM_MOD_NSS
 mv $MOVE ../../resman/ncs/
