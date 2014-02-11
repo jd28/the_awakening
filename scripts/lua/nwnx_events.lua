0157fa55dd452aa03abb1bc93f70a7292b2730df
jmd <jmd2028@gmail.com>
2014-02-11 03:28:22 -0800

    Remove DestroyObject event handler

--- scripts/lua/nwnx_events.lua
+++ scripts/lua/nwnx_events.lua
@@ -1,10 +1,1 @@
 local NXEv = require 'solstice.nwnx.events'
-
-NXEv.RegisterEventHandler(
-   NXEv.EVENT_TYPE_DESTROY_OBJECT,
-   function (ev)
-      local obj = ev.object
-      _SOL_REMOVE_CACHED_OBJECT(obj.id)
-      return false
-   end)
-   
