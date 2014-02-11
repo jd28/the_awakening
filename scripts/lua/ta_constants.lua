fd6894b0b408ccfded898cce0a077b3c0937f28f
jmd <jmd2028@gmail.com>
2014-02-11 08:00:47 -0800

    Add constant script.

---
+++ scripts/lua/ta_constants.lua
@@ -0,0 +1,6 @@
+local Const = require 'solstice.constant'
+
+Const.Register("classes", "Constant")
+Const.Register("ambientsound", "Constant")
+Const.Register("appearance", "Constant")
+Const.Register("area_const", "Constant", nil, "Value", "int")
