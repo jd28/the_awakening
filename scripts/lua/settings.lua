9b540cd2406b1e3130efe5410133a4b334525907
jmd <jmd2028@gmail.com>
2014-02-11 02:20:44 -0800

    lua: Remove unused stuff from settings.

--- scripts/lua/settings.lua
+++ scripts/lua/settings.lua
@@ -11,15 +11,4 @@
 -- REQUIRED: Log directory.
 LOG_DIR = "../logs.0"
 
--- OPTIONAL: Preload file for server specific lua files.
---PRELOAD = "ta.preload"
 
--- OPTIONAL: 
--- If set to false all CEP specific variables, baseitems, etc
--- will be ignored.  If set to a CEP version number without dots
--- (e,g 2.3c -> 23) CEP resources will be available.
---USING_CEP = 23
-
-----------------------------------------------------------------------
--- Fake server specific setting.
---WHATEVER = "whatever"
