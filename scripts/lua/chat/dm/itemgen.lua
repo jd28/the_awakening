76788938437ad1c5350a4c16fa3057d58f069fe1
jmd <jmd2028@gmail.com>
2014-02-12 02:33:51 -0800

    rename dm item generator command

---
+++ scripts/lua/chat/dm/itemgen.lua
@@ -0,0 +1,17 @@
+local Item = require 'solstice.item'
+
+command = "item"
+description = [[Item generator commands.
+               |  generate <resref>
+               |  list]]
+
+function action(chat_info)
+   local act = chat_info.param:split(' ')
+   if not act then return end
+   local pc = chat_info.speaker
+   if act[1] == "generate" and act[2] then
+      Item.Generate(pc, act[2])
+   elseif act[1] == "list" then
+      pc:SendMessage(Item.List())
+   end
+end
