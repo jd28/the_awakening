4161086363955409e5897d9cbc5ac527458ee7dc
jmd <jmd2028@gmail.com>
2014-02-16 01:22:14 -0800

    server: Fix incorrect concat operator...

--- scripts/lua/ta/server.lua
+++ scripts/lua/ta/server.lua
@@ -8,14 +8,16 @@ local M = {}
 function M.VerifyAdmin(obj)
    local cdkey = obj:GetPCPublicCDKey()
    local mod = Game.GetModule()
-   return mod:GetLocalInt("AUTH_"+cdkey) > 0
+   return mod:GetLocalInt("AUTH_" .. cdkey) > 1
 end
 
 --- Verify DM
 -- @param obj Object to test for DM privileges.
 function M.VerifyDM(obj)
    local cdkey = obj:GetPCPublicCDKey()
-   return mod:GetLocalInt("AUTH_"+cdkey) > 1
+   local mod = Game.GetModule()
+   local var = "AUTH_" .. cdkey
+   return mod:GetLocalInt(var) > 0
 end
 
 return M
