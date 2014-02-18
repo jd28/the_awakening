9782cb15d0ab03f4b6400ccfe33818c61f8159d8
jmd <jmd2028@gmail.com>
2014-02-18 02:06:42 -0800

    dynamo: Add some new functions

--- scripts/lua/ta/dynamo.lua
+++ scripts/lua/ta/dynamo.lua
@@ -26,6 +26,34 @@ function M.GetLevelTable(tbl, level)
    return tbl.Default
 end
 
+function M.GetLevelHolder(tbl, obj)
+   local result = tbl
+   if tbl.rotate then
+      result = tbl[tbl.rotate]
+      if tbl.rotate == #tbl then
+         tbl.rotate = 1
+      else
+         tbl.rotate = tbl.rotate + 1
+      end
+   elseif tbl.every then
+      if tbl[tbl.every] then
+         result = tbl[tbl.every]
+      else
+         result = tbl[0]
+      end
+      tbl.every = tbl.every + 1
+   elseif tbl.if_ then
+      result = default
+      for i = 1, #tbl.if_, 2 do
+         if _G[tbl.if_[i]](obj) then
+            result = tbl.if_[i+1]
+            break
+         end
+      end
+   end
+   return result
+end
+
 function M.GetValue(value, use_max)
    if type(value) == "number" then
       return value
@@ -49,6 +77,28 @@ end
 function M.Chance(chance, tbl)
    tbl.chance = chance
    return tbl
+end
+
+function M.Rotate(...)
+   local t = {...}
+   t.rotate = 1
+end
+
+function M.Every(default, ...)
+   local t = { every = 1 }
+   t[0] = default
+
+   local temp = {...}
+   for i=1, #temp, 2 do
+      t[temp[i]] = temp[i+1]
+   end
+
+   return t
+end
+
+function M.If(default, ...)
+   default.if_ = {...}
+   return default
 end
 
 return M
