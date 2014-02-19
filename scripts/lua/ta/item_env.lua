eca9dab867522d8b60dc5caa75584d613751343d
jmd <jmd2028@gmail.com>
2014-02-19 02:34:48 -0800

    items: Update doc strings.

--- scripts/lua/ta/item_env.lua
+++ scripts/lua/ta/item_env.lua
@@ -11,6 +11,7 @@ local Dyn = require 'ta.dynamo'
 local function add_ip(fname, ...)
    local t = {...}
    t.f = fname
+   Dyn.set_base(t)
    return t
 end
 
@@ -56,7 +57,7 @@ function E.BonusFeat(value)
 end
 
 --- Creates a "bonus spell of a specified level" itemproperty.
--- @param class CLASS.* constant
+-- @param class CLASS\_TYPE\_*
 -- @param level [0, 9]
 -- @param n Number of spell slots to add.
 function E.BonusLevelSpell(class, level, n)
@@ -66,28 +67,28 @@ function E.BonusLevelSpell(class, level, n)
 end
 
 --- Creates a "cast spell" itemproperty.
--- @param spell solstice.itemprop.const.CASTSPELL_*
--- @param uses solstice.itemprop.const.CASTSPELL_NUMUSES_*
+-- @param spell IP_CONST_CASTSPELL_*
+-- @param uses IP_CONST_CASTSPELL_NUMUSES_*
 function E.CastSpell(spell, uses)
    return add_ip("CastSpell", spell, uses)
 end
 
 --- Create a "reduced weight container" itemproperty.
--- @param amount CONTAINERWEIGHTRED_*
+-- @param amount IP_CONST_CONTAINERWEIGHTRED_*
 function E.ContainerReducedWeight(amount)
    return add_ip("ContainerReducedWeight", amount)
 end
 
 --- Creates a damage bonus itemproperty.
--- @param damage_type DAMAGE.* constant.
--- @param damage DAMAGE.BONUS_*
+-- @param damage_type DAMAGE\_TYPE\_*
+-- @param damage DAMAGE_BONUS_*
 function E.DamageBonus(damage_type, damage)
    return add_ip("DamageBonus", damage_type, damage)
 end
 
 --- Creates a damage immunity itemproperty.
--- @param damage_type solstice.damage type constant.
--- @param amount solstice.itemprop.const.DAMAGEIMMUNITY_*
+-- @param damage_type DAMAGE\_TYPE\_*
+-- @param amount IP_CONST_DAMAGEIMMUNITY_*
 function E.DamageImmunity(damage_type, amount)
    return add_ip("DamageImmunity", damage_type, amount)
 end
@@ -99,22 +100,22 @@ function E.DamagePenalty(amount)
 end
 
 --- Creates a damage reduction itemproperty.
--- @param enhancement solstice.itemprop.REDUCTION_*
--- @param soak solstice.itemprop.SOAK_*
+-- @param enhancement IP_CONST_REDUCTION_*
+-- @param soak IP_CONST_SOAK_*
 function E.DamageReduction(enhancement, soak)
    return add_ip("DamageReduction", enhancement, soak)
 end
 
 --- Creates damage resistance item property.
--- @param damage_type solstice.damage type constant.
--- @param amount solstice.itemprop.RESIST_*
+-- @param damage_type DAMAGE_TYPE_*
+-- @param amount IP_CONST_RESIST_*
 function E.DamageResistance(damage_type, amount)
    return add_ip("DamageResistance", damage_type, amount)
 end
 
 --- Creates damage vulnerability item property.
--- @param damage_type solstice.damage type constant.
--- @param amount solstice.itemprop.const.DAMAGEVULNERABILITY_*
+-- @param damage_type DAMAGE_TYPE_*
+-- @param amount IP_CONST_DAMAGEVULNERABILITY_*
 function E.DamageVulnerability(damage_type, amount)
    return add_ip("DamageVulnerability", damage_type, amount)
 end
@@ -131,7 +132,7 @@ function E.EnhancementModifier(value)
 end
 
 --- Creates an "extra damage type" item property.
--- @param damage_type DAMAGE.*
+-- @param damage_type DAMAGE\_TYPE\_*
 -- @param[opt=false] is_ranged ExtraRangedDamge if true, melee if false.
 function E.ExtraDamageType(damage_type, is_ranged)
    return add_ip("ExtraDamageType", damage_type, is_ranged)
@@ -159,7 +160,7 @@ function E.HolyAvenger()
 end
 
 --- Creates immunity item property
--- @param immumity_type solstice.itemprop.IMMUNITYMISC_*
+-- @param immumity_type IMMUNITY_TYPE_
 function E.Immunity(immumity_type)
    return add_ip("ImmunityMisc", immumity_type)
 end
@@ -175,14 +176,14 @@ function E.Keen()
 end
 
 --- Creates a light item property.
--- @param brightness LIGHTBRIGHTNESS_*
--- @param color LIGHTCOLOR_*
+-- @param brightness IP_CONST_LIGHTBRIGHTNESS_*
+-- @param color IP_CONST_LIGHTCOLOR_*
 function E.Light(brightness, color)
    return add_ip("Light", brightness, color)
 end
 
 --- Creates a class use limitation item property
--- @param class solstice.class constant
+-- @param class CLASS_TYPE_*
 function E.LimitUseByClass(class)
    return add_ip("LimitUseByClass", class)
 end
@@ -194,7 +195,7 @@ function E.LimitUseByRace(race)
 end
 
 --- Creates a massive criticals item property.
--- @param damage solstice.damage.BONUS_*
+-- @param damage DAMAGE_BONUS_*
 function E.MassiveCritical(damage)
    return add_ip("MassiveCritical", damage)
 end
@@ -213,7 +214,7 @@ function E.Mighty(value)
 end
 
 --- Creates Monster Damage effect.
--- @param damage solstice.itemprop.const.MONSTERDAMAGE_*
+-- @param damage IP_CONST_MONSTERDAMAGE_*
 function E.MonsterDamage(damage)
    return add_ip("MonsterDamage", damage)
 end
@@ -224,7 +225,7 @@ function E.NoDamage()
 end
 
 --- Creates an "on hit cast spell" item property.
--- @param spell solstice.itemprop.const.ONHIT_CASTSPELL_*
+-- @param spell IP_CONST_ONHIT_CASTSPELL_*
 -- @param level Level spell is cast at.
 function E.OnHitCastSpell(spell, level)
    return add_ip("OnHitCastSpell", spell, level)
@@ -232,15 +233,15 @@ end
 
 --- Creates on monster hit item property.
 -- NOTE: Item property is bugged.  See NWN Lexicon.
--- @param prop solstice.itemprop.const.ONMONSTERHIT_*
+-- @param prop IP_CONST_ONMONSTERHIT_*
 -- @param special ???
 function E.OnHitMonster(prop, special)
    return add_ip("OnHitMonster", prop, special)
 end
 
 --- Creates an OnHit itemproperty.
--- @param prop solstice.itemprop.const.ONHIT_*
--- @param dc solstice.itemprop.const.ONHIT_SAVEDC_*
+-- @param prop IP_CONST_ONHIT_*
+-- @param dc IP_CONST_ONHIT_SAVEDC_*
 -- @param special Meaning varies with type. (Default: 0)
 function E.OnHit(prop, dc, special)
    return add_ip("OnHitProps", prop, dc, special)
@@ -253,20 +254,20 @@ function E.Regeneration(amount)
 end
 
 --- Creates quality item property
--- @param quality solstice.itemprop.const.QUALITY_*
+-- @param quality IP_CONST_QUALITY_*
 function E.Quality(quality)
    return add_ip("Quality", quality)
 end
 
 --- Creates saving throw bonus item property
--- @param save_type solstice.itemprop.const.SAVEBASETYPE_*
+-- @param save_type IP_CONST_SAVEBASETYPE_*
 -- @param amount [1,20] or [-20, -1]
 function E.SavingThrow(save, amount)
    return add_ip("SavingThrow", save, amount)
 end
 
 --- Creates saving throw bonus vs item property
--- @param save_type solstice.itemprop.const.SAVEVS_*
+-- @param save_type IP_CONST_SAVEVS_*
 -- @param amount [1,20] or [-20, -1]
 function E.SavingThrowVersus(save, amount)
    return add_ip("SavingThrowVersus", save, amount)
@@ -286,19 +287,19 @@ function E.SpellImmunityLevel(level)
 end
 
 --- Creates an "immunity to specific spell" itemproperty.
--- @param spell solstice.itemprop.const.IMMUNITYSPELL_*
+-- @param spell IP_CONST_IMMUNITYSPELL_*
 function E.SpellImmunitySpecific(spell)
    return add_ip("SpellImmunitySpecific", spell)
 end
 
 --- Creates an "immunity against spell school" itemproperty.
--- @param school solstice.itemprop.const.SPELLSCHOOL_*
+-- @param school IP_CONST_SPELLSCHOOL_*
 function E.SpellImmunitySchool(school)
    return add_ip("SpellImmunitySchool", school)
 end
 
 --- Creates a spell resistince item property
--- @param amount solstice.itemprop.const.SPELLRESISTANCEBONUS_*
+-- @param amount IP_CONST_SPELLRESISTANCEBONUS_*
 function E.SpellResistance(amount)
    return add_ip("SpellResistance", amount)
 end
