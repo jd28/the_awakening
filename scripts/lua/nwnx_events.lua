32d912120e5c943d86b88b9fd6fd3992b79e7a89
jmd <jmd2028@gmail.com>
2014-04-18 01:41:30 -0700

    lua/nwnx_events: Fix remove object from cache function call.

--- scripts/lua/nwnx_events.lua
+++ scripts/lua/nwnx_events.lua
@@ -1,9 +1,11 @@
 local NXEv = require 'solstice.nwnx.events'
+local C = require('ffi').C
 
 NXEv.RegisterEventHandler(
    NXEv.EVENT_TYPE_DESTROY_OBJECT,
    function (ev)
       local obj = ev.object
-      _SOL_REMOVE_CACHED_OBJECT(obj.type, obj.id)
+      _SOL_REMOVE_CACHED_OBJECT(obj.id)
+      C.Local_DeleteCreature(obj.id)
       return false
    end)
