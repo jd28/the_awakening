d576d926dac2b1771251096f9004a8f2696e38e3
jmd <jmd2028@gmail.com>
2014-04-18 01:39:29 -0700

    lua chat commands: Add gc command, add necessary cache functions.

--- scripts/lua/chat/admin/lua_commands.lua
+++ scripts/lua/chat/admin/lua_commands.lua
@@ -1,14 +1,22 @@
 command = "lua"
 description = "Lua related commands"
 
+local safe_require = safe_require
+local package = package
+local collectgarbage = collectgarbage
+local string = string
+
+local System = require 'solstice.system'
+local _SOL_REMOVE_CACHED_OBJECT = _SOL_REMOVE_CACHED_OBJECT
+
 function action(chat_info)
    local act = chat_info.param:split(' ')
    if not act then return end
-   
+
    if act[1] == 'reload' then
       if not package.loaded[act[2]] then
-	 print ("Error: "..act[2].." not loaded!")
-	 chat_info.speaker:SendMessage("Error: "..act[2].." not loaded!")
+         --print ("Error: "..act[2].." not loaded!")
+         chat_info.speaker:SendMessage("Error: "..act[2].." not loaded!")
       end
 
       package.loaded[act[2]] = nil
@@ -16,5 +24,15 @@ function action(chat_info)
 
    elseif act[1] == 'load' then
       safe_require(act[2])
+   elseif act[1] == 'gc' then
+      local before = collectgarbage("count")
+      collectgarbage()
+      local msg = string.format("Lua GC: Before %f, After: %f",
+                                before, collectgarbage("count"))
+      chat_info.speaker:SendMessage(msg)
+   elseif act[1] == "dump_globals" then
+      System.LogGlobalTable()
+   elseif act[1] == "remove" then
+      _SOL_REMOVE_CACHED_OBJECT(chat_info.target.id)
    end
 end
