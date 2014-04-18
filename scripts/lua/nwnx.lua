3ecbb4a2168d406d43302f3e9ffd9fc83a45dce2
jmd <jmd2028@gmail.com>
2014-04-18 01:44:07 -0700

    lua/nwnx: Fix requires.

--- scripts/lua/nwnx.lua
+++ scripts/lua/nwnx.lua
@@ -1,10 +1,9 @@
-safe_require "solstice.nwnx.areas"
 safe_require 'solstice.nwnx.chat'
-safe_require 'solstice.nwnx.combat'
 safe_require "solstice.nwnx.database"
 safe_require "solstice.nwnx.dmactions"
 safe_require "solstice.nwnx.effects"
 safe_require 'solstice.nwnx.equips'
+safe_require 'solstice.nwnx.items'
 safe_require "solstice.nwnx.events"
 safe_require "solstice.nwnx.haks"
 safe_require "solstice.nwnx.levels"
