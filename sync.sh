d437a125cb0fe52b06341923f028e405870c96c9
jmd <jmd2028@gmail.com>
2014-04-27 03:40:20 -0700

    Add updating tlk folder in sync.sh

--- sync.sh
+++ sync.sh
@@ -2,3 +2,4 @@ SSH='ssh -l'
 
 rsync -r -a -v  -e "$SSH $1" --checksum --delete  ./scripts/lua $2:/opt/nwn
 rsync -r -a -v  -e "$SSH $1" --checksum --delete ./resman/ncs $2:/opt/nwn/resman
+rsync -r -a -v  -e "$SSH $1" --checksum ./tlk $2:/opt/nwn/
