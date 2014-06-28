e4f70348287260b9a6f606c2f77273eb28d26f82
jmd <jmd2028@gmail.com>
2014-06-28 08:59:48 -0700

    Fix incorrect itemprop functions.

--- scripts/lua/items/fab_barb_armor.lua
+++ scripts/lua/items/fab_barb_armor.lua
@@ -3,29 +3,29 @@ resref = "fab_barb_armor"
 -- Properties
 properties = {
    ArmorClass(8),
-   Ability(ABILITY_STRENGTH, 4),
-   Ability(ABILITY_CONSTITUTION, 6),
+   AbilityScore(ABILITY_STRENGTH, 4),
+   AbilityScore(ABILITY_CONSTITUTION, 6),
 
-   DamageImmunity(DAMAGE_INDEX_DIVINE, 60),
-   DamageImmunity(DAMAGE_INDEX_MAGICAL, 60),
-   DamageImmunity(DAMAGE_INDEX_NEGATIVE, 60),
-   DamageImmunity(DAMAGE_INDEX_POSITIVE, 60),
-   DamageImmunity(DAMAGE_INDEX_BLUDGEONING, 60),
-   DamageImmunity(DAMAGE_INDEX_PIERCING, 60),
-   DamageImmunity(DAMAGE_INDEX_SLASHING, 60),
+   DamageImmunity(DAMAGE_INDEX_DIVINE, 10),
+   DamageImmunity(DAMAGE_INDEX_MAGICAL, 10),
+   DamageImmunity(DAMAGE_INDEX_NEGATIVE, 10),
+   DamageImmunity(DAMAGE_INDEX_POSITIVE, 10),
+   DamageImmunity(DAMAGE_INDEX_BLUDGEONING, 10),
+   DamageImmunity(DAMAGE_INDEX_PIERCING, 10),
+   DamageImmunity(DAMAGE_INDEX_SLASHING, 10),
 
    DamageResistance(DAMAGE_INDEX_POSITIVE, IP_CONST_RESIST_10),
    DamageResistance(DAMAGE_INDEX_NEGATIVE, IP_CONST_RESIST_10),
    DamageResistance(DAMAGE_INDEX_DIVINE, IP_CONST_RESIST_10),
 
-   Immunity(IMMUNITY_TYPE_DEATH),
+   ImmunityMisc(IMMUNITY_TYPE_DEATH),
    SavingThrowVersus(SAVING_THROW_VS_ALL, 3),
 
    Regeneration(6),
 
-   Skill(SKILL_TAUNT, 8),
-   Skill(SKILL_PARRY, 8),
-   Skill(SKILL_INTIMIDATE, 8),
+   SkillModifier(SKILL_TAUNT, 8),
+   SkillModifier(SKILL_PARRY, 8),
+   SkillModifier(SKILL_INTIMIDATE, 8),
 
    Light(IP_CONST_LIGHTBRIGHTNESS_NORMAL, IP_CONST_LIGHTCOLOR_WHITE),
 }
