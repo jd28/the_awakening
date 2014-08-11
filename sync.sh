f1d8b6e3836a89e80e87aa53ffb3788dee26d260
jmd <jmd2028@gmail.com>
2014-08-11 01:12:04 -0700

    Update sync.sh to send NCS files to resman folder.

--- sync.sh
+++ sync.sh
@@ -1,5 +1,5 @@
 SSH='ssh -l'
 
 rsync -r -a -v  -e "$SSH $1" --checksum --delete  ./scripts/lua $2:/opt/nwn
-rsync -r -a -v  -e "$SSH $1" --checksum --delete ./resman/ncs $2:/opt/nwn/resman
+rsync -r -a -v  -e "$SSH $1" --checksum --include="*.ncs" --exclude="*" scripts/nwn/ncs/ $2:/opt/nwn/resman/ncs/
 rsync -r -a -v  -e "$SSH $1" --checksum ./tlk $2:/opt/nwn/
