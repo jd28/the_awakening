b8ce485baa438dd9a15a48474d463eba46b032fc
jmd <jmd2028@gmail.com>
2014-09-01 03:11:25 -0700

    I'm sick of wasting my time on resman.  Add scripts back to the module.

--- scripts/nwn/compile.sh
+++ scripts/nwn/compile.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 
 NCS="c:/Users/josh/software/NWN/modules/temp0/$1*.ncs"
+MOVE="ncs/$1*.ncs"
+MASK="nss/$1*.nss"
+MOD="c:/Users/josh/software/NWN/modules/temp0"
 
 rm $NCS
-make
+NWNScriptCompiler.exe -b ncs -i "nss;include;bioware" -1cego "$MASK"
+mv $MOVE $MOD
