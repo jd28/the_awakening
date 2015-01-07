bd99c933c9af5c39e8cf2b171bf2c501a73569c3
jmd <jmd2028@gmail.com>
2015-01-07 02:29:23 -0800

    Stop tracking kills for now.

--- scripts/lua/ta_update_kills.lua
+++ scripts/lua/ta_update_kills.lua
@@ -2,6 +2,7 @@ local insert = table.insert
 local fmt = string.format
 
 function ta_update_kills(pc)
+--[[
    if not OPT.dbcon then return end
    if string.find(pc:GetName(), '%[TEST%]') then return end
    local vars = pc:GetAllVars('^kill', VARIABLE_TYPE_INT)
@@ -10,15 +11,9 @@ function ta_update_kills(pc)
                ('%s', '%s', '%s')
                ON DUPLICATE KEY UPDATE
                val='%s';]]
-
-   for k, v in pairs(vars) do
-      if k:starts('killg_') then
-         local res = assert(OPT.dbcon:execute(fmt(s,
-                                                  pc:GetLocalString('pc_player_name'),
-                                                  k,
-                                                  tostring(v),
-                                                  tostring(v))))
-      elseif k:starts('kill_') then
+--[[
+   for k, v in pairs(pc:GetAllProperties()) do
+      if k:starts('kill_') then
          local res = assert(OPT.dbcon:execute(fmt(s,
                                                   pc:GetTag(),
                                                   k,
@@ -26,28 +21,20 @@ function ta_update_kills(pc)
                                                   tostring(v))))
       end
    end
+   --]]
 end
 
 function ta_load_kills(pc)
+--[[
    local s = fmt("SELECT name,val from pwdata where `name` LIKE '%%kill_%%' AND tag='%s'",
                  pc:GetTag())
    local cur = assert(OPT.dbcon:execute(s))
    local i = cur:numrows()
    local row = cur:fetch({}, 'a')
    while row and i > 0 do
-      pc:SetLocalInt(row.name, tonumber(row.val))
+      pc:SetProperty(row.name, tonumber(row.val))
       row = cur:fetch(row, 'a')
       i = i - 1
    end
-
-   s = fmt("SELECT name,val from pwdata where `name` LIKE '%%killg_%%' AND tag='%s'",
-           pc:GetLocalString('pc_player_name'))
-   cur = assert(OPT.dbcon:execute(s))
-   i = cur:numrows()
-   row = cur:fetch({}, 'a')
-   while row and i > 0 do
-      pc:SetLocalInt(row.name, tonumber(row.val))
-      row = cur:fetch(row, 'a')
-      i = i - 1
-   end
+   --]]
 end
