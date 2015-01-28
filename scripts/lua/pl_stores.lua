8ec565b7ece2c8c69273b1d4afe45f025a6d35c0
jmd <jmd2028@gmail.com>
2015-01-28 11:32:08 -0800

    Fix some whitespace issues.

--- scripts/lua/pl_stores.lua
+++ scripts/lua/pl_stores.lua
@@ -4,9 +4,10 @@ function st_test_area(store)
    local t = {'fab_barb_armor', 'fab_druid_helm'}
 
    for _, v in ipairs(t) do
-   if not store:HasItem(v) then
-      local it = Item.Generate(store, v)
-      assert(it:GetIsValid())
-      it:SetInfiniteFlag(true)
+      if not store:HasItem(v) then
+         local it = Item.Generate(store, v)
+         assert(it:GetIsValid())
+         it:SetInfiniteFlag(true)
+      end
    end
 end
