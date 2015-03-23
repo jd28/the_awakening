69b973e00d9d00f26eb4726e547e623d6acaf123
jmd <jmd2028@gmail.com>
2015-03-23 06:21:15 -0700

    Move 'scripts' into subdirectory and update to new Game module script functions.

--- scripts/lua/preload.lua
+++ scripts/lua/preload.lua
@@ -54,20 +54,13 @@ then
 
 end
 
-for f in lfs.dir("./"..script_dir) do
-   if f ~= "preload.lua" and
-      f ~= "settings.lua" and
-      f ~= OPT.CONSTANTS .. '.lua' and
-      string.find(f:lower(), ".lua", -4)
-   then
-      local file = fmt('%s/%s/%s', lfs.currentdir(), script_dir, f)
+for f in lfs.dir("./"..script_dir..'/scripts/') do
+   if string.find(f:lower(), ".lua", -4) then
+      local file = fmt('%s/%s/%s', lfs.currentdir(), script_dir..'/scripts/', f)
       log:info("Loading: " .. file)
-
-      -- Wrap the dofile call in a pcall so that errors can be logged here
-      -- and so that they will not cause the for loop to abort.
-      local result, err = pcall(function() dofile(file) end)
-      if not result then
-         log:error("ERROR Loading: %s : %s", file, err)
-      end
+      Game.LoadScript(file)
    end
 end
+
+-- Lock the script environment so we can avoid undeclared variables.
+Game.LockScriptEnvironment()
