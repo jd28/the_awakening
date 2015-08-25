86550bf54a64eba8e2a0c09d4d7351b540b7770a
jmd <jmd2028@gmail.com>
2015-08-25 03:38:57 -0700

    Don't delete on rsync.

--- sync.sh
+++ sync.sh
@@ -2,6 +2,6 @@
 
 SSH='ssh -l'
 
-rsync -r -a -v  -e "$SSH $1" --checksum --delete  ./scripts/lua $2:/opt/nwn
+rsync -r -a -v  -e "$SSH $1" --checksum ./scripts/lua $2:/opt/nwn
 rsync -r -a -v  -e "$SSH $1" --include="*.ncs" --exclude="*" scripts/nwn/ncs/ $2:/opt/nwn/physfs/
 rsync -r -a -v  -e "$SSH $1" --checksum ./tlk $2:/opt/nwn/
