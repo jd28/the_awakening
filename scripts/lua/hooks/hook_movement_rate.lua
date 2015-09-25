e849364bbabc972e0696a3330e5c74d8d51f3fb5
jmd <jmd2028@gmail.com>
2015-09-25 03:45:35 -0700

    Fix typos and whitespace.

--- scripts/lua/hooks/hook_movement_rate.lua
+++ scripts/lua/hooks/hook_movement_rate.lua
@@ -13,15 +13,19 @@ local function Hook_GetMovementRateFactor(obj)
     if can and level > 3 then
       mo = 0.2
     end
+
     if cre:GetLevelByClass(CLASS_TYPE_BARBARIAN) >= 1 then
       ba = 0.1
     end
+
     ta = (cre['TA_MOVE_SPEED'] or 0) / 100
-    end
+
     local mr = obj.cre_move_rate
     if mr > 1.5 then mr = 1.5 end
     return mr + ta + max(mo, ba)
+
   end
+
   return Orig_GetMovementRateFactor(cre)
 end
 
