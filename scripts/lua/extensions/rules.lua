7f105d1b9a1c69eff2a4803176183aaa0eac9c62
jmd <jmd2028@gmail.com>
2016-01-13 13:11:28 -0800

    Add GetMaxHitPoint override.

--- scripts/lua/extensions/rules.lua
+++ scripts/lua/extensions/rules.lua
@@ -142,5 +142,61 @@ local function GetWeaponPower(cre, item)
   return power
 end
 
+local function GetMaxHitPoints(cre)
+  if cre.ta_clone then
+    return cre.obj.obj.obj_hp_max
+  end
+  local res = 0
+  local not_pc = cre:GetIsAI()
+  local level = cre:GetHitDice()
+  if cre:GetHasFeat(FEAT_TOUGHNESS) then
+    res = res + level
+  end
+
+  res = res + math.max(0, cre:GetAbilityModifier(ABILITY_CONSTITUTION) * level)
+
+  local pm = cre:GetLevelByClass(CLASS_TYPE_PALE_MASTER)
+  local pmhp = 0
+  if pm >= 5 then
+    if pm >= 25 then
+      pmhp = 18 + (math.floor(pm / 5) * 20)
+    elseif pm >= 15 then
+      pmhp = 18 + (math.floor(pm / 5) * 10)
+    elseif pm >= 10 then
+      pmhp = 18 + math.floor((pm - 10) / 5)
+    else
+      pmhp = pm * 3
+    end
+  end
+  res = res + pmhp
+
+
+  if cre:GetHasFeat(FEAT_EPIC_TOUGHNESS_LEG) then
+    res = res + 0
+  elseif cre:GetHasFeat(FEAT_EPIC_TOUGHNESS_SUP) then
+    res = res + 0
+  elseif cre:GetHasFeat(FEAT_EPIC_TOUGHNESS) then
+    res = res + 0
+  end
+
+  res = res + cre['SOL_HP_EFF']
+
+  if not_pc then
+    res = res + cre.obj.obj.obj_hp_max
+  else
+    local base = 0
+    for i = 1, cre:GetHitDice() do
+      base = base + cre:GetMaxHitPointsByLevel(i)
+    end
+    res = res + base
+    cre.obj.obj.obj_hp_max = res
+  end
+
+  if res <= 0 then res = 1 end
+
+  return res
+end
+
 Rules.GetWeaponAttackBonus = GetWeaponAttackBonus
 Rules.GetWeaponPower = GetWeaponPower
+Rules.GetMaxHitPoints = GetMaxHitPoints
